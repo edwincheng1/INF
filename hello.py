0fe58115e494865027e10e4d3328e0d22531378c
edwin cheng <2601615@sit.singaporetech.edu.sg>
2026-09-18 11:50:20 +0800

    Changes to be committed: 	modified:   hello.py 	modified:   modular_auditor.py

--- hello.py
+++ hello.py
@@ -1,3 +1,46 @@
+'''
+Activity 1
+
+print('==========================')
+print('Welcome Here')
+print('My First Post!')
+print('==========================')
+
+Activity 2
+print('==========================')
+print('Welcome Here')
+print('My First Post!')
+print('==========================')
+
+username = 'cool_creator'
+bio = 'Fun Blogger'
+followers = 100
+
+print('username: ',username)
+print('Bio: ',bio)
+print('Followers: ',followers)
+
+Activity 3
+followers = 100
+followers += 50
+print('Day 1: ',followers)
+followers += 20
+print('Day 2: ',followers)
+followers += 10
+print('Day 3: ',followers)
+
+Activity 4
+username = input('Enter Username: ')
+age = input('Enter Age: ')
+category = input('Enter Content Category: ')
+
+print('\nInstagram Profile')
+print('====================')
+print('Username: ',username)
+print('Age: ',age)
+print('Category: ',category)'''
+
+#Activity 5
 username = input('Enter Username: ')
 age = int(input('Enter Age: '))
 category = input('Enter Content Category: ')
--- modular_auditor.py
+++ modular_auditor.py
@@ -1,10 +1,26 @@
 stock = 0
 failed = 0
+
+def get_valid_input():
+    p = 1
+
+def process_delivery(current_total, new_value):
+    new_value = new_value + current_total
+    return new_value
+
+def calculate_tax(amount):
+    tax = amount * 0.1
+    tax = f"{tax:.2f}"
+    return tax
+
+def generate_report(total_units, failed_attempts):
+    print("Total Deliveries Processed:", total_units)
+    print("Number of Failed/Rejected Entries:", failed_attempts)
+
 while True:
     stockinput = input("Input stock quantity or type 'quit': ")
     if stockinput.lower() == "quit":
-        print("Total Units Processed:", stock)
-        print("Number of Failed/Rejected Entries:", failed)
+        generate_report(stock, failed)
         break
     elif stockinput.startswith("-"):
         print("Invalid input. Please enter a positive number.")
@@ -12,12 +28,10 @@ while True:
     elif not stockinput.isdigit():
         print("Invalid input. Please enter an integer.")
         failed += 1
-
     else:
         stockcount = int(stockinput)
         stock = stock + stockcount
-        if stock > 500:
-            print("Total inventory is more than 500. End of program.")
-            break
-        else:
-            print("Total inventory stock:", stock)
+        calculate_tax(stockcount)
+        if stock > 0:
+            print("Number of deliveries processed:", stock)
+            print("Tax:",calculate_tax(stockcount))
